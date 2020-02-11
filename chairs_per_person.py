3e5cb2b6c94831bc60d36d19a4391fe52f08ed37
Hasson Khaled Alali <57362009+hassonalali@users.noreply.github.com>
2020-02-11 15:31:05 +0100

    Update chairs_per_person.py

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,11 @@
-f = open("reservations.csv")
+f = open("reservations")
 
 for reservation in f:
     name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
+    try:
+        chairs_per_person = 50 / int(number)
+    except ValueError: 
+        break
     print("{} will get {} chairs per person".format(name, chairs_per_person))
+
+
